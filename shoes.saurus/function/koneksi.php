9f9cbaf90812ed113a60880bafdb17cc3b678dc3
Bagus Anom <71835691+bagusanom@users.noreply.github.com>
2020-11-26 23:24:07 +0800

    Delete koneksi.php

--- shoes.saurus/function/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "shoes.saurus";
-
-$koneksi = mysqli_connect($server, $username, $password, $database) or die("Gagal terhubung ke database");
